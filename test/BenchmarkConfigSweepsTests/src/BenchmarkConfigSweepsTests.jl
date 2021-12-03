45b3615a287441a7860d58b70c4f705a6ba24d65
Takafumi Arakaki <aka.tkf@gmail.com>
2021-12-02 21:19:57 -0800

    Use Aqua (#16)

--- test/BenchmarkConfigSweepsTests/src/BenchmarkConfigSweepsTests.jl
+++ test/BenchmarkConfigSweepsTests/src/BenchmarkConfigSweepsTests.jl
@@ -3,5 +3,6 @@ module BenchmarkConfigSweepsTests
 include("test_configurations.jl")
 include("test_info.jl")
 include("test_examples.jl")
+include("test_aqua.jl")
 
 end  # module BenchmarkConfigSweepsTests
